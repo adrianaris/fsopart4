aba036546c65fb630c48912c92f7a008b2259786
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-11-11 18:18:51 +0100

    middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const config = require('./utils/config')
 const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/bloglist')
+const middleware = require('./utils/middleware')
+const morgan = require('morgan')
 
 logger.info(`connecting to ${config.MONGODB_URI}`)
 
@@ -18,8 +20,17 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 app.use(cors())
+app.use(express.static('buid'))
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+
+morgan.token('POST', (req) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length - :response-time ms :POST'))
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
