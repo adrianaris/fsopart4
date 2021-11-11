85f39ff7f0232445d9ed15df8a4a808e333a3423
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-11-11 15:47:45 +0100

    logger

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
+const logger = require('./utils/logger')
+
+console.log(`connecting to ${config.MONGODB_URI}`)
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+    
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server runnning in port ${PORT}`)
+})
