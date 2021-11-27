c49f287bc6356c41c4f36cc4b391b7316958bd67
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-11-27 16:30:28 +0100

    4.20

--- controllers/bloglist.js
+++ controllers/bloglist.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-
-    return null
-}
 
 blogRouter.get('/', async (request, response) =>{
     const bloglist = await Blog
@@ -21,7 +13,7 @@ blogRouter.get('/', async (request, response) =>{
 })
 
 blogRouter.post('/', async (request, response) => {
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing  or invalid' })
